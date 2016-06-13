dd8189b591af8d42b55a083bab4e33c35a95ed13
tianqiubiao <tianqiubiao@xjfbank.com>
2016-06-13 17:21:00 +0800

    html

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,20 @@
 /*
-* #news_about  媒体报道分页
-* #sell_money_list  定期理财feny
-* .money_item_list
-* #other_money_list 他人转让分页列表
-* */
+ * #news_about  媒体报道分页
+ * #sell_money_list  定期理财feny
+ * .money_item_list
+ * #other_money_list 他人转让分页列表
+ * */
 
 var regex = /[\da-zA-Z]{6,20}/
-var phoneReg = /^(13[0-9]|14[0-9]|15[0-9]|18[0-9])\d{8}$/;
-
-
+//var phoneReg = /^(13[0-9]|14[0-9]|15[0-9]|18[0-9]|17[0-9])\d{8}$/;
+var phoneReg=/^(1[3-5|7-8]\d)\d{8}$/;
 /*
-* a:点击id
-* b:显示父级id
-* c：显示元素
-* d:点击颜色
-*
-* */
+ * a:点击id
+ * b:显示父级id
+ * c：显示元素
+ * d:点击颜色
+ *
+ * */
 
 function code(a, b, c, d, e) {
     var ulList = document.getElementById(a), ulLists = ulList.getElementsByTagName(e);
@@ -54,8 +53,8 @@ function code(a, b, c, d, e) {
 }
 
 /*
-* 理财点击切换
-* */
+ * 理财点击切换
+ * */
 itemList("money_item_list", 'bgOrange');
 /*分页切换*/
 itemList('fyList', 'fyListBg')
@@ -100,7 +99,8 @@ $('#true_qd').click(function () {
                             return;
                         }
 
-                    })
+                    }
+)
 function accountQianDao() {
     var timer = window.setTimeout(function () {
                                       $('#my_account01Top').css('display', 'none');
@@ -114,38 +114,42 @@ function accountQianDao() {
 }
 /*弹出登录框*/
 
-$('.sign').click(function(){
-    if($(this).text()==='登录'||'已有账号？马上登录'){
-        $('.sign_on').css('display','block')
-    }
-})
+$('.sign').click(function () {
+                     if ($(this).text() === '登录' || '已有账号？马上登录') {
+                         $('.sign_on').css('display', 'block')
+                     }
+                 }
+)
 /*弹出框*/
-$('.sign_close').click(function(e){
-   $('.sign_on').css('display','none')
-})
+$('.sign_close').click(function (e) {
+                           $('.sign_on').css('display', 'none')
+                       }
+)
 
 /*注册事件*/
-$('.trueDisplay>span').click(function () {
-                                 if ($(this).children()[0].alt === 'false') {
-                                     $(this).children().css('display', 'block');
-                                     $(this).children()[0].alt = 'true';
-                                     var that = $(this);
-                                     $('.true_sign_on').click(function () {
-                                                                  if (that.children()[0].alt === 'true' && phoneReg.test($(this).val())) {
+$('.spanImg_seco').click(function () {
+                             if ($(this).children()[0].alt === 'false') {
+                                 $(this).children().css('display', 'block');
+                                 $(this).children()[0].alt = 'true';
+                                 var that = $(this);
 
+                                 $('.true_sign_on').click(function () {
+                                                              if (that.children('img')[0].alt === "true" && phoneReg.test($('#sing0').val())) {
 
-                                                                      alert(1);
-                                                                      //发送请求
-                                                                  } else {
-                                                                      alert('2')
-                                                                  }
-                                                              })
-                                 } else {
-                                     $(this).children().css('display', 'none');
-                                     $(this).children()[0].alt = 'false';
-                                     $('.true_sign_on').unbind('click');
-                                 }
-})
+                                                                  window.location.href='zhucechenggong.html';
+                                                                  //发送请求
+                                                              } else {
+                                                                  alert('2')
+                                                              }
+                                                          }
+                                 )
+                             } else {
+                                 $(this).children().css('display', 'none');
+                                 $(this).children()[0].alt = 'false';
+                                 $('.true_sign_on').unbind('click');
+                             }
+                         }
+)
 
 /*密码验证*/
 $('input').blur(function (e) {
@@ -161,37 +165,220 @@ $('input').blur(function (e) {
                     } else if (this.id === "sing1") {
                         var that = $(this);
                         var a = $('#passW')
-                        passWord(that, a)
+                        //passWord(that, a)
 
                     } else if (this.id === "sing2") {
                         var a = $('#passWT')
                         var that = $(this);
-                        passWord(that, a)
+                        //passWord(that, a)
                     } else if (this.id === "sing3") {
 
                     } else if (this.id === "sing4") {
 
                     }
-})
+                }
+)
 function passWord(that, a) {
 
     if (that.val().length === 0) {
         a.html('请输入密码！')
-    } else if (that.val().length<=12 &&that.val().length>=6 ) {
+    } else if (that.val().length <= 12 && that.val().length >= 6) {
         if (regex.test(that.val())) {
             a.html('密码不符合要求')
-        }else {
+        } else {
             a.html('2')
         }
-    }else if(! (that.val().length >= 6 && that.val().length <= 12)){
+    } else if (!(that.val().length >= 6 && that.val().length <= 12)) {
         a.html('密码长度不符合！ ')
-    }else if (a === $('#passWT')) {
+    } else if (a === $('#passWT')) {
         that.val() === $('#sing1').val() ? a.html('') : a.html('两次密码输入不一致！')
-    }else {
+    } else {
         a.html('1')
     }
 }
 /*进入账户*/
-$('#account_in').click(function(){
-    window.location.href='w-wodezhnghu.html'
+$('#account_in').click(function () {
+                           window.location.href = 'w-wodezhnghu.html'
+                       }
+)
+function dataHref() {
+    $("*[data-href]").click(function () {
+                                var url = $(this).attr("data-href");
+                                var target = $(this).attr("data-target");
+                                if (url != "") {
+                                    if (target == "_blank") {
+                                        window.open(url, target);
+                                        loading();
+                                    } else if (target == "_self") {
+                                        window.open(url, target);
+                                        loading();
+                                    } else if (target == "_parent") {
+                                        window.open(url, target);
+                                        loading();
+                                    } else if (target == "_top") {
+                                        window.open(url, target);
+                                        loading();
+                                    } else {
+                                        window.location.href = url;
+                                        loading();
+                                    }
+                                } else {
+                                    return false;
+                                }
+                            }
+    );
+}
+dataHref()
+
+/*进度条*/
+function bfb_deco() {
+    $('.bfb_yuan').each(function () {
+var that=$(this)
+        var yuanCenter=$(this).find('.yuan_center');
+        var yuanTop=$(this).find('.yuan_top')
+        var bfb_J = $(this).find('.bfb_J').get(0).innerText;
+                            $(this).find('.height_yuan').css('height', a(bfb_J));
+                            function a(bfb_J) {
+                                switch (bfb_J) {
+                                    case '已满标':
+                                         border();
+                                        that.find('.bfb_J').css('color','#FFE98D');
+                                        return '0%';
+                                        break;
+                                    case '已结束':
+                                        return function(){
+                                           that.css('border','2px solid #eee');
+                                            yuanCenter.css('background','#ccc');
+                                            yuanTop.css('background','#eee')
+                                            return '0'
+                                        };
+                                        break;
+                                    case '还款中':
+                                        border();
+                                        return '0%';
+                                        break;
+                                    default:
+                                        border();
+                                        return (100- parseInt(bfb_J))+"%";
+                                        break;
+                                }
+                                function border(){
+                                   that.css('border','2px solid #CDF0F5');
+                                   yuanCenter.css('background','#6ed3d6');
+                                    yuanTop.css('background','#CDF0F5')
+
+                                }
+                            }
+    });
+    $('.zr .lcFD').each(function(){
+        var reg= /^(\d||\d+)\/(\d||\d+)(份)$/
+        var bfb_J = $(this).find('.bfb_J').get(0).innerText;
+        var fang=$(this).find('.bfb_fang');
+        var fang_span=$(this).find('.weight_fang');
+        var lcFDF=$(this).find('.lcFDF');
+        var lcFDQ =$(this).find('.lcFDQ');
+        fang_span.css('',a());
+        function a() {
+          if(bfb_J==='已结束'){
+              fang.css('border','2px solid #eee');
+              fang_span.css('background','#ccc');
+              lcFDF.css('display','none');
+              lcFDQ.get(0).className='lcFDQ posrel';
+              fang_span.css({'background':'#ccc','width':'100%'});
+          }else {
+              if(reg.test(bfb_J)){
+                  fang.css('border','2px solid #CDF0F5');
+                  fang_span.css({'background':'#6dd3d7','width':(reg.exec(bfb_J)[1]/reg.exec(bfb_J)[2])*100+'%'});
+              }else {
+                  lcFDF.css('display','none');
+                  lcFDQ.get(0).className='lcFDQ posrel';
+              }
+
+          }
+        }
+    })
+    $('.moneyCenter .mCLi6').each(function(){
+        var that=$(this);
+        var mcLi7=that.next('.mCLi7');
+        var mcLi7Num=mcLi7.find('p').get(0).innerText;
+        var mcLi6Height=that.find('sellHeight');
+
+        var sell_height_num=that.find('.sellHeightNum').get(0).innerText;
+        that.find('.sellHeight').css('height',function(){
+            switch(mcLi7Num){
+                case '已满标':
+                    mcLi7.addClass('bgYellow');
+                    return '0';
+                break;
+                case '还款中':
+                    mcLi7.addClass('bgGrey');
+                    return '0';
+                break;
+                case '已结束':
+                    mcLi7.addClass('bgGrey');
+                    that.children().first().addClass('bgGrey').removeClass('bgGreen');
+                    return '0';
+                    break;
+                default:
+                    mcLi7.addClass('bgPink');
+                    return (100- parseInt(sell_height_num))+"%";
+                break;
+            }
+        })
+    })
+}
+bfb_deco();
+/*千分符*/
+function splits(str){
+    var str2=str.toString().split(".")[1]
+    str2= typeof (str2)==="undefined"?'.00':'.'+str2
+   return str.toString().split(".")[0].split("").reverse().join("").replace(/(\d{3}(?!$))/g,"$1,").split('').reverse().join('')+str2;
+}
+
+/*分页控件*/
+function Pages(){
+
+
+
+
+
+
+
+}
+/*退出*/
+$('.clearCook').click(function(){
+var cookie=window.getCookie();
+    cookie.clear();
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
